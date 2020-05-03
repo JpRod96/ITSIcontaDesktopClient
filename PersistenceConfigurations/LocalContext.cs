0b4284d70bf8c24de9c8a36a7c7dcfe81bcd1c99
JpRod96 <jprodriguez60@gmail.com>
2020-05-03 16:14:48 -0400

    Add DB context configuration class LocalContext

---
+++ PersistenceConfigurations/LocalContext.cs
@@ -0,0 +1,18 @@
+﻿using ITSIContaDesktopClient.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSIContaDesktopClient.PersistenceConfigurations
+{
+    class LocalContext : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        private string connectionString = String.Format("Data Source = {0}.db", Global.DB_NAME);
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite(connectionString);
+    }
+}
