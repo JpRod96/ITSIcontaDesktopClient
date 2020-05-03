0038c9d81970e74582519384bd8993123cee29c4
JpRod96 <jprodriguez60@gmail.com>
2020-05-03 16:14:18 -0400

    Add User and Role entities

---
+++ Entities/User/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSIContaDesktopClient.Entities.User
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string password { get; set; }
+        public Role Role { get; set; }
+    }
+}
