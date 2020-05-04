928f452f21a20dfdbbc59d5cd2f81fabec9ca6ef
JpRod96 <jprodriguez60@gmail.com>
2020-05-03 20:48:25 -0400

    Add UserService class for user data persistence and synchronization

--- Global.cs
+++ Global.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,7 @@ namespace ITSIContaDesktopClient
     class Global
     {
         public const string DB_NAME = "ITSIconta";
+        public static string APIToken = "";
+        public static bool isConnected = true;
     }
 }
---
+++ Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using ITSIContaDesktopClient.PersistenceConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSIContaDesktopClient.Services
+{
+    class UserService
+    {
+        private readonly LocalContext LocalStorage;
+        public UserService()
+        {
+            LocalStorage = new LocalContext();
+        }
+
+
+    }
+}
