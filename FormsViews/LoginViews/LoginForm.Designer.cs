d00ebbc81f7321c5a1aad1763c664007d4c071af
JpRod96 <jprodriguez60@gmail.com>
2020-05-07 15:45:27 -0400

    Add new MaterialSkin module, for a more modern form layout

--- FormsViews/LoginViews/LoginForm.Designer.cs
+++ FormsViews/LoginViews/LoginForm.Designer.cs
@@ -28,21 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.presIconPicBox = new System.Windows.Forms.PictureBox();
+            this.userNameTxtBox = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.passwordTxtBox = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            ((System.ComponentModel.ISupportInitialize)(this.presIconPicBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // presIconPicBox
+            // 
+            this.presIconPicBox.Image = global::ITSIContaDesktopClient.Properties.Resources.presentationIcon;
+            this.presIconPicBox.Location = new System.Drawing.Point(69, 99);
+            this.presIconPicBox.Name = "presIconPicBox";
+            this.presIconPicBox.Size = new System.Drawing.Size(260, 260);
+            this.presIconPicBox.TabIndex = 4;
+            this.presIconPicBox.TabStop = false;
+            // 
+            // userNameTxtBox
+            // 
+            this.userNameTxtBox.BackColor = System.Drawing.SystemColors.Control;
+            this.userNameTxtBox.Depth = 0;
+            this.userNameTxtBox.Hint = "";
+            this.userNameTxtBox.Location = new System.Drawing.Point(106, 395);
+            this.userNameTxtBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.userNameTxtBox.Name = "userNameTxtBox";
+            this.userNameTxtBox.PasswordChar = '\0';
+            this.userNameTxtBox.SelectedText = "";
+            this.userNameTxtBox.SelectionLength = 0;
+            this.userNameTxtBox.SelectionStart = 0;
+            this.userNameTxtBox.Size = new System.Drawing.Size(176, 23);
+            this.userNameTxtBox.TabIndex = 1;
+            this.userNameTxtBox.UseSystemPasswordChar = false;
+            this.userNameTxtBox.Click += new System.EventHandler(this.UserNameTxtBox_Click);
+            this.userNameTxtBox.Leave += new System.EventHandler(this.UserNameTxtBox_Leave);
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.BackColor = System.Drawing.SystemColors.Control;
+            this.passwordTxtBox.Depth = 0;
+            this.passwordTxtBox.Hint = "";
+            this.passwordTxtBox.Location = new System.Drawing.Point(106, 434);
+            this.passwordTxtBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.PasswordChar = '\0';
+            this.passwordTxtBox.SelectedText = "";
+            this.passwordTxtBox.SelectionLength = 0;
+            this.passwordTxtBox.SelectionStart = 0;
+            this.passwordTxtBox.Size = new System.Drawing.Size(176, 23);
+            this.passwordTxtBox.TabIndex = 2;
+            this.passwordTxtBox.UseSystemPasswordChar = false;
+            this.passwordTxtBox.Click += new System.EventHandler(this.PasswordTxtBox_Click);
+            this.passwordTxtBox.Leave += new System.EventHandler(this.PasswordTxtBox_Leave);
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(400, 500);
+            this.Controls.Add(this.passwordTxtBox);
+            this.Controls.Add(this.userNameTxtBox);
+            this.Controls.Add(this.presIconPicBox);
             this.Name = "LoginForm";
             this.Text = "Iniciar Sesión";
             this.Shown += new System.EventHandler(this.Login_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.presIconPicBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox presIconPicBox;
+        private MaterialSkin.Controls.MaterialSingleLineTextField userNameTxtBox;
+        private MaterialSkin.Controls.MaterialSingleLineTextField passwordTxtBox;
     }
 }
 
--- FormsViews/LoginViews/LoginForm.cs
+++ FormsViews/LoginViews/LoginForm.cs
@@ -1,4 +1,7 @@
-﻿using ITSIContaDesktopClient.FormsViews.LoginViews;
+﻿using ITSIContaDesktopClient.Controllers;
+using ITSIContaDesktopClient.FormsViews.LoginViews;
+using MaterialSkin;
+using MaterialSkin.Controls;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,20 +14,83 @@ using System.Windows.Forms;
 
 namespace ITSIContaDesktopClient
 {
-    public partial class LoginForm : Form
+    public partial class LoginForm : MaterialForm
     {
+        private readonly UserController userController;
+        private const string EMPTY_TEXT = "";
+        private const string USERNAME_DEFAULT_TEXT = "Nombre de usuario";
+        private const string PASSWORD_DEFAULT_TEXT = "Contraseña";
+        private string passwordContent;
+
         public LoginForm()
         {
-            this.StartPosition = FormStartPosition.CenterScreen;
+            userController = new UserController();
             InitializeComponent();
+            BeforeShown();
         }
 
+        private void BeforeShown()
+        {
+            MaterialConfig();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
+            presIconPicBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            userNameTxtBox.Text = USERNAME_DEFAULT_TEXT;
+            passwordTxtBox.Text = PASSWORD_DEFAULT_TEXT;
+            passwordContent = "";
+        }
 
+        private void MaterialConfig()
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
 
         private void Login_Shown(object sender, EventArgs e)
         {
+            CheckForConnection();
+        }
+
+        private void CheckForConnection()
+        {
             CheckingConnectionForm checkingConnectionDLG = new CheckingConnectionForm();
             checkingConnectionDLG.ShowDialog();
+        }
+
+        private void UserNameTxtBox_Click(object sender, EventArgs e)
+        {
+            TextBoxPlaceholderContent(userNameTxtBox, USERNAME_DEFAULT_TEXT, true);
+        }
+
+        private void PasswordTxtBox_Click(object sender, EventArgs e)
+        {
+            TextBoxPlaceholderContent(passwordTxtBox, PASSWORD_DEFAULT_TEXT, true);
+        }
+
+        private void UserNameTxtBox_Leave(object sender, EventArgs e)
+        {
+            TextBoxPlaceholderContent(userNameTxtBox, USERNAME_DEFAULT_TEXT, false);
+        }
+
+        private void PasswordTxtBox_Leave(object sender, EventArgs e)
+        {
+            TextBoxPlaceholderContent(passwordTxtBox, PASSWORD_DEFAULT_TEXT, false);
+        }
+
+        private void TextBoxPlaceholderContent(MaterialSingleLineTextField textBox, string defaultText, bool isClicking)
+        {
+            string content = textBox.Text;
+            if (isClicking)
+            {
+                content = content.Equals(defaultText) ? EMPTY_TEXT : content;
+            }
+            else
+            {
+                content = content.Equals(EMPTY_TEXT) ? defaultText : content;
+            }
+            textBox.Text = content;
         }
     }
 }
